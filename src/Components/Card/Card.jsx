7b9778a62db40d09636f998c3d4bee59c6eefeb7
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2023-12-24 03:46:13 +0530

    updated icons

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -12,8 +12,6 @@ function Card({list}) {
         dispatch(add(item));
     }
 
-    
-
     return (
         list.map((item) => (
             <div key={Math.random()} id={item.id} className=' tw-card w-40 h-fit flex  flex-col rounded-xl m-2  text-center relative group overflow-hidden bg-white '>
@@ -25,13 +23,11 @@ function Card({list}) {
         
                 </div>
                 <button onClick={()=>handelAdd(item)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute top-1 right-1 hover:fill-yellow-400 text-yellow-500">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                    </svg>
-                </button>
 
-        
-                
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute top-1 right-1 hover:fill-yellow-400 text-yellow-400">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                </svg >
+                </button>
             </div>
         )
     )
