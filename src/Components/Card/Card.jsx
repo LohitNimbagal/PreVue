5cb98ed0fc001807740054d6d35f9c25fe5fa082
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2024-01-03 11:20:35 +0530

    Updated card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,11 +7,7 @@ import { addId } from '../../Store/detailsSlice'
 
 
 function Card({list}) {
-
-    
-    
     const placeholder = 'https://placehold.co/400x600'
-    
     const dispatch = useDispatch();
     
     const handelAdd = (item)=>{
@@ -36,10 +32,9 @@ function Card({list}) {
         
                 </div>
                 <button onClick={()=>handelAdd(item)}>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute top-1 right-1 hover:fill-yellow-400 text-yellow-400">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-                </svg >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute top-1 right-1 hover:fill-yellow-400 text-yellow-400">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                    </svg >
                 </button>
             </div>
         )
