9311da2acb5b44d49077e57a5314e4f36eb374e3
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2024-01-16 18:20:20 +0530

    fixed card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -25,7 +25,7 @@ function Card({list}) {
         <>
             {
                 list.map((item) => (
-                    <div key={item.id} id={item.id} className='tw-card w-52 m-1 h-fit flex flex-col rounded-xl text-center relative group overflow-hidden bg-white hover:cursor-pointer'>
+                    <div key={item.id} id={item.id} className='w-40 m-1 h-fit flex flex-col rounded-xl text-center relative group overflow-hidden bg-white hover:cursor-pointer lg:w-52'>
                         <img src={item.poster_path ? `https://image.tmdb.org/t/p/w500/${item.poster_path}` : placeholderImage} alt="" className='rounded-xl' onClick={() => handleCardClick(item)} />
                         <div className='absolute h-1/4 text-white w-full flex items-center justify-center bg-black/50 -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300 '>
                             <h2 className='text-xs flex justify-center items-center font-bold'>{item.title ? item.title : item.name}</h2>
