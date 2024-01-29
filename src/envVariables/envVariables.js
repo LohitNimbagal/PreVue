cf758e35cbdc8f14b6799c898148a4d327377cb1
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2024-01-29 17:14:13 +0530

    fixed api

--- src/envVariables/envVariables.js
+++ src/envVariables/envVariables.js
@@ -1,5 +1,5 @@
 const envVariables = {
-    apiKey: String(import.meta.env.VITE_TMDB_API_KEY),
+    apiKey: String(import.meta.env.VITE_TMDB_API),
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
